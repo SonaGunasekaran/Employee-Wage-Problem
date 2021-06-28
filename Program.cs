d560c172c84fa8038272308ff55f7deb7d63213c
[Sona] <[sona16061999@gmail.com]>
2021-06-28 20:35:53 +0530

    Employee Computation

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeeComputation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to EmployeeWage Computation Program on Master Branch");
+        }
+    }
+}
