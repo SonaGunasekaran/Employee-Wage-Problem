160b6ad8894a9d4d9e04e49e6e5e8cf0ee86a3bb
[Sona] <[sona16061999@gmail.com]>
2021-07-03 14:57:10 +0530

    Employee Wage for Two Companies

---
+++ EmployeeWage.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeComputation
+{
+    public class EmployeeWage
+    {
+        public const int FULL_TIME = 1;
+        public const int PART_TIME = 2;
+        public const int EMP_PER_HOUR = 20;
+        
+
+
+        public static int ComputingWage(string companyName,int maxWorkingDay,int maxWorkingHr)
+        {
+            //local variable
+            int empHour = 0;
+            int empWage = 0;
+            int totalWage = 0;
+            int workingDays = 1;
+            int workingHours = 0;
+
+
+            Random random = new Random();
+
+            //calling a Next() method
+
+            int empInput = random.Next(0, 3);
+
+            //calculating totalWages
+
+            while (workingDays <= maxWorkingDay && workingHours <= maxWorkingHr)
+            {
+                switch (empInput)
+                {
+                    case FULL_TIME:
+
+                        empHour = 8;
+                        break;
+
+                    case PART_TIME:
+
+
+                        empHour = 4;
+                        break;
+
+                    default:
+                        empHour = 0;
+                        break;
+
+                }
+
+               //Formula for calculate Employee Wage
+
+                empWage = EMP_PER_HOUR * empHour;
+
+                workingHours += empHour;
+
+                totalWage += empWage;
+
+                if (empInput != 0)
+                {
+                    workingDays++;
+                }
+
+
+            }
+
+            Console.WriteLine("In"+" "+ companyName + " "+"Total Employee Wage for maximum " + " " + workingDays + " " + "days = " + " " + totalWage);
+            
+            return totalWage;
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -4,67 +4,11 @@ namespace EmployeeComputation
 {
     public class Program
     {
-        public const int FULL_TIME = 1;
-        public const int PART_TIME = 2;
-        public const int EMP_PER_HOUR = 20;
-        public const int MAX_WORKING_DAYS = 20;
-        public const int MAX_WORKING_HOURS = 100;
-
         public static void Main(string[] args)
         {
-
-            ComputingWage();
-            //ComputingWage("Tvs Next", 10, 20, 100);
-
-        }
-
-
-
-        public static void ComputingWage()
-        {
-            int empHour = 0;
-            int empWage = 0;
-            int totalWage = 0;
-            int workingDays = 1;
-            int workingHours = 0;
-
-
-            Random random = new Random();
-            int empInput = random.Next(0, 3);
-            while (workingDays <= MAX_WORKING_DAYS && workingHours <= MAX_WORKING_HOURS)
-            {
-                switch (empInput)
-                {
-                    case FULL_TIME:
-
-                        empHour = 8;
-                        break;
-
-                    case PART_TIME:
-
-
-                        empHour = 4;
-                        break;
-
-                    default:
-                        empHour = 0;
-                        break;
-
-                }
-
-
-                empWage = EMP_PER_HOUR * empHour;
-                workingHours += empHour;
-                totalWage += empWage;
-                if (empInput != 0)
-                {
-                    workingDays++;
-                }
-
-
-            }
-
-            Console.WriteLine("Total Employee Wage for maximum " + " " + workingDays + " " + "days = " + " " + totalWage);
+            
+            EmployeeWage.ComputingWage("Amazon", 20, 100);
+            EmployeeWage.ComputingWage("Sirius", 30, 100);
         }
     }
 }
