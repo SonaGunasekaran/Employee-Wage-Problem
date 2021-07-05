c79721788beac1d814189394d6454b52292403c0
[Sona] <[sona16061999@gmail.com]>
2021-07-05 13:02:46 +0530

    Daily for Multiple Companies

--- EmpComputeWage.cs
+++ EmpComputeWage.cs
@@ -14,6 +14,7 @@ namespace EmployeeComputation
         public int maxWorkingDay;
         public int maxHrPerMonth;
         public int totalWage;
+        private List<int> dailywage;
 
         public EmpComputeWage(string companyName, int ratePerHour, int maxWorkingDay, int maxHrPerMonth)
         {
@@ -21,14 +22,31 @@ namespace EmployeeComputation
             this.ratePerHour = ratePerHour;
             this.maxHrPerMonth = maxHrPerMonth;
             this.maxWorkingDay = maxWorkingDay;
+            dailywage = new List<int>();
         }
         public void setTotalEmpWage(int totalWage)
         {
             this.totalWage = totalWage;
         }
+
+        public void SetDailyWage(int totalWage , EmpComputeWage empComputeWage)
+        {
+            empComputeWage.dailywage.Add(totalWage);
+        }
+
         public string toString()
         {
             return "Total Employee wage for Company " + this.companyName + " is " + this.totalWage;
+        }
+
+        public void DailyWage(EmpComputeWage empComputeWage)
+        {
+            int day = 1;
+            foreach (int totalWage in empComputeWage.dailywage)
+            {
+                Console.WriteLine("Day{0}:{1}", day, totalWage);
+                day++;
+            }
         }
     }
 }
